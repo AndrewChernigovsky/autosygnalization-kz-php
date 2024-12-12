df5a27443db378a03d9d92b49638ef97a8217345
Girey <larsvokul@mail.ru>
2024-12-12 19:55:28 +0300

    Переделал autosygnal

--- src/files/php/pages/autosygnals/auto.php
+++ src/files/php/pages/autosygnals/auto.php
@@ -1,64 +1,59 @@
 <section class="autosygnals">
-  <div class="container">
-    <div class="autosygnals__wrapper">
-      <h3 class="autosygnals__title">Автосигнализации</h3>
-      <ul class="autosygnals__list list-style-none">
-        <li class="autosygnals__item">
-          <img class="autosygnals__item-image" src="/dist/assets/images/services/service-1.png"
-            alt="Auto Security.Автосигнализация">
-          <h3 class="autosygnals__item-title">Автосигнализации с автозапуском</h3>
-          <div class="autosygnals__item-footer">
-            <p class="autosygnals__item-count">
-              <span class="autosygnals__item-counter">11</span> товаров
-            </p>
-            <a class="autosygnals__item-link link y-button-primary" href="#">ПЕРЕЙТИ В РАЗДЕЛ</a>
-          </div>
-        </li>
-        <li class="autosygnals__item">
-          <img class="autosygnals__item-image" src="/dist/assets/images/services/service-2.png"
-            alt="Auto Security.Автосигнализация">
-          <h3 class="autosygnals__item-title">Автосигнализации с GSM</h3>
-          <div class="autosygnals__item-footer">
-            <p class="autosygnals__item-count">
-              <span class="autosygnals__item-counter">8</span> товаров
-            </p>
-            <a class="autosygnals__item-link link y-button-primary" href="#">ПЕРЕЙТИ В РАЗДЕЛ</a>
-          </div>
-        </li>
-        <li class="autosygnals__item">
-          <img class="autosygnals__item-image" src="/dist/assets/images/services/service-3.png"
-            alt="Auto Security.Автосигнализация">
-          <h3 class="autosygnals__item-title">Автосигнализации без автозапуска</h3>
-          <div class="autosygnals__item-footer">
-            <p class="autosygnals__item-count">
-              <span class="autosygnals__item-counter">3</span> товаров
-            </p>
-            <a class="autosygnals__item-link link y-button-primary" href="#">ПЕРЕЙТИ В РАЗДЕЛ</a>
-          </div>
-        </li>
-        <li class="autosygnals__item">
-          <img class="autosygnals__item-image" src="/dist/assets/images/services/service-4.png"
-            alt="Auto Security.Автосигнализация">
-          <h3 class="autosygnals__item-title">Каталог автосигнализаций Starline</h3>
-          <div class="autosygnals__item-footer">
-            <p class="autosygnals__item-count">
-              <span class="autosygnals__item-counter">8</span> товаров
-            </p>
-            <a class="autosygnals__item-link link y-button-primary" href="#">ПЕРЕЙТИ В РАЗДЕЛ</a>
-          </div>
-        </li>
-        <li class="autosygnals__item">
-          <img class="autosygnals__item-image" src="/dist/assets/images/services/service-5.png"
-            alt="Auto Security.Автосигнализация">
-          <h3 class="autosygnals__item-title">Пульты и аксессуары</h3>
-          <div class="autosygnals__item-footer">
-            <p class="autosygnals__item-count">
-              <span class="autosygnals__item-counter">15</span> товаров
-            </p>
-            <a class="autosygnals__item-link link y-button-primary" href="#">ПЕРЕЙТИ В РАЗДЕЛ</a>
-          </div>
-        </li>
-      </ul>
+    <div class="container">
+        <div class="autosygnals__wrapper">
+            <h3 class="autosygnals__title">Автосигнализации</h3>
+            <ul class="autosygnals__list list-style-none">
+                <li class="autosygnals__item">
+                    <h3 class="autosygnals__item-title">Автосигнализации с автозапуском</h3>
+                    <div class="autosygnals__item-block"
+                        style="background-image: url('/dist/assets/images/services/service-1.png')">
+                        <a class="autosygnals__item-count link y-button-secondary" href="#">
+                            <span class="autosygnals__item-counter">11</span> товаров
+                        </a>
+                        <a class="autosygnals__item-link link y-button-primary" href="#">ПЕРЕЙТИ В РАЗДЕЛ</a>
+                    </div>
+                </li>
+                <li class="autosygnals__item">
+                    <h3 class="autosygnals__item-title">Автосигнализации с GSM</h3>
+                    <div class="autosygnals__item-block"
+                        style="background-image: url('/dist/assets/images/services/service-2.png')">
+                        <a class="autosygnals__item-count link y-button-secondary" href="#">
+                            <span class="autosygnals__item-counter">8</span> товаров
+                        </a>
+                        <a class="autosygnals__item-link link y-button-primary " href="#">ПЕРЕЙТИ В РАЗДЕЛ</a>
+                    </div>
+                </li>
+                <li class="autosygnals__item">
+                    <h3 class="autosygnals__item-title">Автосигнализации без автозапуска</h3>
+                    <div class="autosygnals__item-block"
+                        style="background-image: url('/dist/assets/images/services/service-3.png')">
+                        <a class="autosygnals__item-count link y-button-secondary" href="#">
+                            <span class="autosygnals__item-counter">3</span> товаров
+                        </a>
+                        <a class="autosygnals__item-link link y-button-primary" href="#">ПЕРЕЙТИ В РАЗДЕЛ</a>
+                    </div>
+                </li>
+                <li class="autosygnals__item">
+                    <h3 class="autosygnals__item-title">Каталог автосигнализаций Starline</h3>
+                    <div class="autosygnals__item-block"
+                        style="background-image: url('/dist/assets/images/services/service-4.png')">
+                        <a class="autosygnals__item-count link y-button-secondary" href="#">
+                            <span class="autosygnals__item-counter">8</span> товаров
+                        </a>
+                        <a class="autosygnals__item-link link y-button-primary" href="#">ПЕРЕЙТИ В РАЗДЕЛ</a>
+                    </div>
+                </li>
+                <li class="autosygnals__item">
+                    <h3 class="autosygnals__item-title">Пульты и аксессуары</h3>
+                    <div class="autosygnals__item-block"
+                        style="background-image: url('/dist/assets/images/services/service-5.png')">
+                        <a class="autosygnals__item-count link y-button-secondary" href="#">
+                            <span class="autosygnals__item-counter">15</span> товаров
+                        </a>
+                        <a class="autosygnals__item-link link y-button-primary" href="#">ПЕРЕЙТИ В РАЗДЕЛ</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
     </div>
-  </div>
 </section>
