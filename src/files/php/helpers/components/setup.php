19519f5806316e1bf1f14e161fd50fcea7169878
MamkinCoder <aleksey.aleksandrovich.mc@gmail.com>
2024-12-14 20:45:22 +0300

    create html

--- src/files/php/helpers/components/setup.php
+++ src/files/php/helpers/components/setup.php
@@ -1,1 +1,16 @@
-<h1>SETUP</h1>
+<section class='setup'>
+    <div class='setup__container'>
+        <div class='setup__wrapper'>
+            <h2 class='setup__title'>Установочный центр</h2>
+            <p class='setup__description'>Воспользуйтесь услугами нашего сервисного центра</p>
+            <a class='setup__link'>Прайс на оборудование и установку</a>
+            <button class='setup__btn button' type='button'>В раздел</button>
+        </div>
+        <div class='setup__img-container'>
+            <picture>
+                <source type="image/png" media="(min-width: 768px)" srcset="./assets/images/setup/setup-img.png" width="700" height="554">
+                <img src="./assets/images/setup/setup-img-mobile.png" width='300' height='350' alt="Серсис">
+            </picture>
+        </div>
+    </div>
+</section>
