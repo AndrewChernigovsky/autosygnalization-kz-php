92b30fd225ded142eebcfa247f293d694cbe037d
Izosimov_Andrew <chernigovsky108@gmail.com>
2024-12-15 20:42:09 +0300

    fixies

--- src/files/php/pages/autosygnals/auto.php
+++ src/files/php/pages/autosygnals/auto.php
@@ -1,88 +1,38 @@
 <?php
 include_once __DIR__ . '/../../helpers/classes/setVariables.php';
+include_once __DIR__ . '/../../data/navigation-links.php';
+
 $variables = new SetVariables();
 $variables->setVar();
 $path = $variables->getPathFileURL();
+$autosygnals = $categories_autosygnals;
 ?>
 
 <section class="autosygnals" id="autosygnals">
-    <div class="container">
-        <h2 class="autosygnals__title">Автосигнализации</h2>
-        <div class="autosygnals__wrapper swiper">
-            <ul class="autosygnals__list list-style-none swiper-wrapper">
-                <li class="autosygnals__item swiper-slide">
-                    <div class="autosygnals__item-card">
-                        <h3 class="autosygnals__item-title">АВТОСИГНАЛИЗАЦИИ С АВТОЗАПУСКОМ</h3>
-                        <img class="autosygnals__item-image" src="/dist/assets/images/autosygnals/autosygnals-1.png"
-                            alt="Картинка на которой изображена услуга Auto Security Автосигнализации с автозапуском"
-                            width="640" height="554">
-                        <div class="autosygnals__item-block">
-                            <p class="autosygnals__item-count">
-                                <span class="autosygnals__item-counter">11</span> товаров
-                            </p>
-                            <a class="autosygnals__item-link link y-button-primary" href="#">В РАЗДЕЛ</a>
-                        </div>
-                    </div>
-                </li>
-                <li class="autosygnals__item swiper-slide">
-                    <div class="autosygnals__item-card">
-                        <h3 class="autosygnals__item-title">АВТОСИГНАЛИЗАЦИИ С GSM</h3>
-                        <img class="autosygnals__item-image" src="/dist/assets/images/autosygnals/autosygnals-2.png"
-                            alt="Картинка на которой изображена услуга Auto Security Автосигнализации с автозапуском"
-                            width="640" height="554">
-                        <div class="autosygnals__item-block">
-                            <p class="autosygnals__item-count">
-                                <span class="autosygnals__item-counter">11</span> товаров
-                            </p>
-                            <a class="autosygnals__item-link link y-button-primary" href="#">В РАЗДЕЛ</a>
-                        </div>
-                    </div>
-                </li>
-                <li class="autosygnals__item swiper-slide">
-                    <div class="autosygnals__item-card">
-                        <h3 class="autosygnals__item-title">АВТОСИГНАЛИЗАЦИИ БЕЗ АВТОЗАПУСКА</h3>
-                        <img class="autosygnals__item-image" src="/dist/assets/images/autosygnals/autosygnals-3.png"
-                            alt="Картинка на которой изображена услуга Auto Security Автосигнализации с автозапуском"
-                            width="640" height="554">
-                        <div class="autosygnals__item-block">
-                            <p class="autosygnals__item-count">
-                                <span class="autosygnals__item-counter">11</span> товаров
-                            </p>
-                            <a class="autosygnals__item-link link y-button-primary" href="#">В РАЗДЕЛ</a>
-                        </div>
-                    </div>
-                </li>
-                <li class="autosygnals__item swiper-slide">
-                    <div class="autosygnals__item-card">
-                        <h3 class="autosygnals__item-title">КАТАЛОГ АВТОСИГНАЛИЗАЦИЙ STARLINE</h3>
-                        <img class="autosygnals__item-image" src="/dist/assets/images/autosygnals/autosygnals-4.png"
-                            alt="Картинка на которой изображена услуга Auto Security Автосигнализации с автозапуском"
-                            width="640" height="554">
-                        <div class="autosygnals__item-block">
-                            <p class="autosygnals__item-count">
-                                <span class="autosygnals__item-counter">11</span> товаров
-                            </p>
-                            <a class="autosygnals__item-link link y-button-primary" href="#">В РАЗДЕЛ</a>
-                        </div>
-                    </div>
-                </li>
-                <li class="autosygnals__item swiper-slide">
-                    <div class="autosygnals__item-card">
-                        <h3 class="autosygnals__item-title">ПУЛЬТЫ И АКСЕССУАРЫ</h3>
-                        <img class="autosygnals__item-image" src="/dist/assets/images/autosygnals/autosygnals-5.png"
-                            alt="Картинка на которой изображена услуга Auto Security Автосигнализации с автозапуском"
-                            width="640" height="554">
-                        <div class="autosygnals__item-block">
-                            <p class="autosygnals__item-count">
-                                <span class="autosygnals__item-counter">11</span> товаров
-                            </p>
-                            <a class="autosygnals__item-link link y-button-primary" href="#">В РАЗДЕЛ</a>
-                        </div>
-                    </div>
-                </li>
-            </ul>
-        </div>
+  <div class="container">
+    <h2 class="autosygnals__title">Автосигнализации</h2>
+    <div class="autosygnals__wrapper swiper swiper-autosygnals">
+      <ul class="autosygnals__list list-style-none swiper-wrapper">
+        <?php foreach ($autosygnals as $slide): ?>
+          <li class="autosygnals__item swiper-slide">
+            <div class="autosygnals__item-card">
+              <h3 class="autosygnals__item-title"><?php echo $slide['name'] ?></h3>
+              <img class="autosygnals__item-image" src="<?php echo $slide['src'] ?>"
+                alt="Картинка на которой изображена услуга Auto Security Автосигнализации с автозапуском" width="640"
+                height="554">
+              <div class="autosygnals__item-block">
+                <p class="autosygnals__item-count">
+                  <span class="autosygnals__item-counter"><?php echo $slide['count'] ?></span> товаров
+                </p>
+                <a class="autosygnals__item-link link y-button-primary" href="<?php echo $slide['link'] ?>">В РАЗДЕЛ</a>
+              </div>
+            </div>
+          </li>
+        <?php endforeach; ?>
+      </ul>
     </div>
+  </div>
+  </div>
 </section>
 <?php
 include_once __DIR__ . '/../../helpers/components/setup.php';
