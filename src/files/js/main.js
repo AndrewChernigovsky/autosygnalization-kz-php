8dc05e3a4559b1c5367bfe88719c58de4b2e2401
Antonina Sorokan <atretyakova28@yandex.ru>
2024-12-16 17:22:01 +0300

    Добавляет стили для формы и валидацию

--- src/files/js/main.js
+++ src/files/js/main.js
@@ -4,12 +4,14 @@ async function loadModule() {
   const { initFormValidation } = await import("./modules/validation.js");
   const { initSearch } = await import("./modules/search.js");
   const { toggleList } = await import("./modules/footer-menu.js");
+  const { validateSectionForm } = await import("./modules/validate-form.js");
 
   toToggleMenu()
   initSwiper();
   initFormValidation();
   initSearch();
   toggleList();
+  validateSectionForm();
 }
 
 document.addEventListener('DOMContentLoaded', loadModule)
--- src/files/php/sections/form.php
+++ src/files/php/sections/form.php
@@ -1,11 +1,11 @@
 <section class="form" id="form">
   <div class="form__wrapper">
     <h2 class="form__title">Форма обратной связи</h2>
-    <form class="form__main-form" action="#" method="post" id="form">
+    <form class="form__main-form" action="#" method="post" id="feedback-form">
       <fieldset class="form__fieldset">
         <legent class="form__subtitle">Марка:</legent>
         <div class="form__group-radio">
-          <input class="form__radio form__radio--genesis" type="radio" name="mark" id="genesis">
+          <input class="form__radio form__radio--genesis" type="radio" name="mark" id="genesis" checked>
           <label class="visually-hidden" for="genesis">Genesis</label>
           <input class="form__radio form__radio--kia" type="radio" name="mark" id="kia">
           <label class="visually-hidden" for="kia">Kia</label>
@@ -27,17 +27,17 @@
         </li>
         <li class="form__item">
           <label class="form__subtitle">Год выпуска:
-            <input class="form__input" type="number" name="release-year" id="release-year" placeholder="2024" required>
+            <input class="form__input" type="number" name="release-year" id="release-year" placeholder="2024" pattern="\d{4}" required>
           </label>
         </li>
         <li class="form__item">
           <label class="form__subtitle">Имя:
-            <input class="form__input" type="text" name="name" id="name" placeholder="Ivanov Ivan Ivanovich" required>
+            <input class="form__input" type="text" name="name" id="name" placeholder="Ivanov Ivan Ivanovich" pattern="[A-Za-z\s]+" required>
           </label>
         </li>
         <li class="form__item">
           <label class="form__subtitle">Телефон:
-            <input class="form__input" type="tel" name="user-tel" id="user-tel" placeholder="+7 (777) 77 77 777" required>
+            <input class="form__input" type="tel" name="phone" id="phone" placeholder="+7 (777) 77 77 777" pattern="[0-9]+" required>
           </label>
         </li>
         <li class="form__item form__item--textarea">
