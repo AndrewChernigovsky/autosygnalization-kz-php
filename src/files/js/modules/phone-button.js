ff6ae9c06d7f9f9069a244793ade1b1294528498
Izosimov_Andrew <chernigovsky108@gmail.com>
2024-12-23 16:28:34 +0300

    popup

--- src/files/js/modules/phone-button.js
+++ src/files/js/modules/phone-button.js
@@ -1,4 +1,5 @@
 const phoneButton = document.querySelector('.phone-button');
+const phoneButtonWrapper = document.querySelector('.phone-button__wrapper');
 const modalForm = document.querySelector('.modal-form');
 const closeButton = document.querySelector('#modal-form-close');
 
@@ -6,10 +7,14 @@ export function initPhone() {
   if (phoneButton && modalForm) {
     phoneButton.addEventListener('click', () => {
       modalForm.classList.add('active');
+      phoneButtonWrapper.classList.add('active');
+      phoneButton.classList.remove('animated-calling')
     })
 
     closeButton.addEventListener('click', () => {
       modalForm.classList.remove('active');
+      phoneButtonWrapper.classList.remove('active');
+      phoneButton.classList.add('animated-calling')
     })
   }
 }
--- src/files/php/sections/popups/modal-form.php
+++ src/files/php/sections/popups/modal-form.php
@@ -1,11 +1,36 @@
-<div class="popup modal-form">
+<?php
+include_once __DIR__ . '/../../helpers/classes/setVariables.php';
+include_once __DIR__ . '/../../data/contacts.php';
+
+$variables = new SetVariables();
+$variables->setVar();
+$path = $variables->getPathFileURL();
+?>
+
+<div class="popup modal-form active">
   <button type="button" id="modal-form-close">
     <span class="visually-hidden">
       Закрыть форму
     </span>
   </button>
   <div class="container">
-    <h3></h3>
-    <p></p>
+    <div class="popup__phones">
+      <?php
+      if (!empty($phones)) {
+        foreach ($phones as $phone) {
+          $cleanedPhone = str_replace(' ', '', $phone['phone']);
+          echo '<a href="tel:' . htmlspecialchars($cleanedPhone) . '">' . 'Позвонить ' . '<span>' . htmlspecialchars($phone['phone']) . '</span>' . '</a>';
+        }
+      }
+      ?>
+    </div>
+    <div class="popup__icons">
+      <a href="">
+        <img src="<?= $path . '/assets/images/icons/telegram.avif'; ?>" alt="">
+      </a>
+      <a href="https://clck.ru/3FQ7aJ">
+        <img src="<?= $path . '/assets/images/icons/whatsapp.avif'; ?>" alt="">
+      </a>
+    </div>
   </div>
 </div>
