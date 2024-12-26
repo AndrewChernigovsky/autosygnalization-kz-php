1caf9182155e05eb905d7a32086efc36e983d374
Izosimov_Andrew <chernigovsky108@gmail.com>
2024-12-26 23:22:35 +0300

    card tabs to php

--- src/files/php/data/tabs.php
+++ src/files/php/data/tabs.php
@@ -6,64 +6,73 @@ $variables->setVar();
 
 $path = $variables->getPathFileURL();
 
+function generateID()
+{
+  return uniqid();
+}
+
 $tabs = [
   [
+    "id" => generateID(),
     'title' => 'ОПИСАНИЕ',
     'items' => [
       [
         'title' => 'title 1',
         'description' => 'description 1',
-        'path-icon' => 'icon-path-1',
+        'path-icon' => "$path/assets/images/vectors/thermometer.svg",
       ],
       [
         'title' => 'title 2',
         'description' => 'description 2',
-        'path-icon' => 'icon-path-2',
+        'path-icon' => "$path/assets/images/vectors/thermometer.svg",
       ]
     ]
   ],
   [
+    "id" => generateID(),
     'title' => 'ХАРАКТЕРИСТИКИ',
     'items' => [
       [
         'title' => 'title 3',
         'description' => 'description 3',
-        'path-icon' => 'icon-path-3',
+        'path-icon' => "$path/assets/images/vectors/thermometer.svg",
       ],
       [
         'title' => 'title 4',
         'description' => 'description 4',
-        'path-icon' => 'icon-path-4',
+        'path-icon' => "$path/assets/images/vectors/thermometer.svg",
       ]
     ]
   ],
   [
+    "id" => generateID(),
     'title' => 'ГАРАНТИЯ',
     'items' => [
       [
         'title' => 'title 5',
         'description' => 'description 5',
-        'path-icon' => 'icon-path-5',
+        'path-icon' => "$path/assets/images/vectors/thermometer.svg",
       ],
       [
         'title' => 'title 6',
         'description' => 'description 6',
-        'path-icon' => 'icon-path-6',
+        'path-icon' => "$path/assets/images/vectors/thermometer.svg",
       ]
     ]
   ],
   [
+    "id" => generateID(),
     'title' => 'СООТВЕТСТВУЮЩИЕ ТОВАРЫ',
     'items' => [
       [
         'title' => 'title 7',
         'description' => 'description 7',
-        'path-icon' => 'icon-path-7',
+        'path-icon' => "$path/assets/images/vectors/thermometer.svg",
       ],
       [
         'title' => 'title 8',
         'description' => 'description 8',
-        'path-icon' => 'icon-path-8',
+        'path-icon' => "$path/assets/images/vectors/thermometer.svg",
       ]
     ]
   ],
--- src/files/php/sections/card-tabs.php
+++ src/files/php/sections/card-tabs.php
@@ -1,60 +1,40 @@
+<?php
+include_once __DIR__ . '/../helpers/classes/setVariables.php';
+include_once __DIR__ . '/../data/tabs.php';
+
+$variables = new SetVariables();
+$variables->setVar();
+
+$path = $variables->getPathFileURL();
+
+error_log(print_r($tabs, true) . ": TABS");
+
+function isActiveClass()
+{
+  return 'tab__button tab__button--active y-button-secondary';
+}
+?>
+
 <section class="tab">
   <div class="tab__wrapper">
     <div class="tab__buttons">
-      <button class="tab__button tab__button--active y-button-secondary" type="button" data-tab="description">ОПИСАНИЕ</button>
-      <button class="tab__button y-button-secondary" type="button" data-tab="characteristics">ХАРАКТЕРИСТИКИ</button>
-      <button class="tab__button y-button-secondary" type="button" data-tab="warranty">ГАРАНТИЯ</button>
-      <button class="tab__button y-button-secondary" type="button" data-tab="related-products">СООТВЕТСТВУЮЩИЕ ТОВАРЫ</button>
+      <?php foreach ($tabs as $tab): ?>
+        <button type="button" class="<?= isActiveClass() ?>"
+          data-tab="<?php echo $tab['id']; ?>"><?= $tab['title'] ?></button>
+      <?php endforeach; ?>
     </div>
     <div class="tab__content">
-      <ul class="tab__list tab__list--show list-style-none" data-content="description">
-        <li class="tab__item">
-          <h3 class="tab__title">РАСШИРЕННЫЙ ДИАПАЗОН ТЕМПЕРАТУР</h3>
-          <p class="tab__description">StarLine уверенно работает в суровых климатических условиях при температуре от минус 40 до плюс 85 °С благодаря высококачественным комплектующим</p>
-        </li>
-        <li class="tab__item">
-          <h3 class="tab__title">РАСШИРЕННЫЙ ДИАПАЗОН ТЕМПЕРАТУР</h3>
-          <p class="tab__description">StarLine уверенно работает в суровых климатических условиях при температуре от минус 40 до плюс 85 °С благодаря высококачественным комплектующим</p>
-        </li>
-        <li class="tab__item">
-          <h3 class="tab__title">РАСШИРЕННЫЙ ДИАПАЗОН ТЕМПЕРАТУР</h3>
-          <p class="tab__description">StarLine уверенно работает в суровых климатических условиях при температуре от минус 40 до плюс 85 °С благодаря высококачественным комплектующим</p>
-        </li>
-      </ul>
-      <ul class="tab__list list-style-none" data-content="characteristics">
-        <li class="tab__item">
-          <h3 class="tab__title">РАСШИРЕННЫЙ ДИАПАЗОН ТЕМПЕРАТУР</h3>
-          <p class="tab__description">StarLine уверенно работает в суровых климатических условиях при температуре от минус 40 до плюс 85 °С благодаря высококачественным комплектующим</p>
-        </li>
-        <li class="tab__item">
-          <h3 class="tab__title">РАСШИРЕННЫЙ ДИАПАЗОН ТЕМПЕРАТУР</h3>
-          <p class="tab__description">StarLine уверенно работает в суровых климатических условиях при температуре от минус 40 до плюс 85 °С благодаря высококачественным комплектующим</p>
-        </li>
-        <li class="tab__item">
-          <h3 class="tab__title">РАСШИРЕННЫЙ ДИАПАЗОН ТЕМПЕРАТУР</h3>
-          <p class="tab__description">StarLine уверенно работает в суровых климатических условиях при температуре от минус 40 до плюс 85 °С благодаря высококачественным комплектующим</p>
-        </li>
-        <li class="tab__item">
-          <h3 class="tab__title">РАСШИРЕННЫЙ ДИАПАЗОН ТЕМПЕРАТУР</h3>
-          <p class="tab__description">StarLine уверенно работает в суровых климатических условиях при температуре от минус 40 до плюс 85 °С благодаря высококачественным комплектующим</p>
-        </li>
-      </ul>
-      <ul class="tab__list list-style-none" data-content="warranty">
-        <li class="tab__item">
-          <h3 class="tab__title">РАСШИРЕННЫЙ ДИАПАЗОН ТЕМПЕРАТУР</h3>
-          <p class="tab__description">StarLine уверенно работает в суровых климатических условиях при температуре от минус 40 до плюс 85 °С благодаря высококачественным комплектующим</p>
-        </li>
-        <li class="tab__item">
-          <h3 class="tab__title">РАСШИРЕННЫЙ ДИАПАЗОН ТЕМПЕРАТУР</h3>
-          <p class="tab__description">StarLine уверенно работает в суровых климатических условиях при температуре от минус 40 до плюс 85 °С благодаря высококачественным комплектующим</p>
-        </li>
-      </ul>
-      <ul class="tab__list list-style-none" data-content="related-products">
-        <li class="tab__item">
-          <h3 class="tab__title">РАСШИРЕННЫЙ ДИАПАЗОН ТЕМПЕРАТУР</h3>
-          <p class="tab__description">StarLine уверенно работает в суровых климатических условиях при температуре от минус 40 до плюс 85 °С благодаря высококачественным комплектующим</p>
-        </li>
-      </ul>
+      <?php foreach ($tabs as $tab): ?>
+        <ul class="tab__list tab__list--show list-style-none" data-content="<?= $tab['id']; ?>">
+          <?php foreach ($tab['items'] as $item): ?>
+            <li class="tab__item"
+              style="background-image: url('<?php echo isset($item['path-icon']) ? htmlspecialchars($item['path-icon']) : '' ?>')">
+              <h3 class="tab__title"><?= $item['title']; ?></h3>
+              <p class="tab__description"><?= $item['description'] ?></p>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+      <?php endforeach; ?>
     </div>
   </div>
 </section>
