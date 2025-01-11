0b2765fda7c36efc5c3bd11238f55701f08fff5d
Antonina Sorokan <atretyakova28@yandex.ru>
2025-01-11 22:06:30 +0300

    Adds styles of form 2

--- src/files/js/components/Checkout/CheckoutForm.jsx
+++ src/files/js/components/Checkout/CheckoutForm.jsx
@@ -32,10 +32,11 @@ export class CheckoutForm extends Component {
       selectedClientType: value,
       isCompany: value === '2', // Проверяем, является ли выбранный тип "Юридическое лицо"
     });
-  };
+  }; 
 
   render() {
-    const { isCompany, selectedClientType } = this.state; // Деструктурируем состояние для удобства
+    const { isCompany, selectedClientType} =
+      this.state; // Деструктурируем состояние для удобства
 
     return html`
       <p class="checkout-form__title">Оформление заказа</p>
@@ -82,7 +83,7 @@ export class CheckoutForm extends Component {
         </label>
       </fieldset>
       <fieldset>
-        <legend>Адрес доставки</legend>
+        <legend>Адрес доставки:</legend>
         <label>
           <p>Улица*</p>
           <input type="text" required name="street" />
@@ -91,21 +92,23 @@ export class CheckoutForm extends Component {
           <p>Индекс*</p>
           <input type="text" required name="index" />
         </label>
-        <label>
-          <p>Дом*</p>
-          <input type="text" required name="house" />
-        </label>
-        <label>
-          <p>Корпус*</p>
-          <input type="text" required name="corpus" />
-        </label>
-        <label>
-          <p>Квартира*</p>
-          <input type="text" required name="apartment" />
-        </label>
+        <div>
+          <label>
+            <p>Дом*</p>
+            <input type="text" required name="house" />
+          </label>
+          <label>
+            <p>Корпус*</p>
+            <input type="text" required name="corpus" />
+          </label>
+          <label>
+            <p>Квартира*</p>
+            <input type="text" required name="apartment" />
+          </label>
+        </div>
       </fieldset>
       <fieldset>
-        <legend>Данные клиента</legend>
+        <legend>Данные клиента:</legend>
         <label>
           <p>Ваше имя*</p>
           <input type="text" required name="user-name" />
@@ -129,70 +132,95 @@ export class CheckoutForm extends Component {
           <${CheckoutCompany} />
         </fieldset>
       `}
-      <fieldset>
-        <legend>Способ доставки</legend>
-        <label>
-          <p>Быстрая доставка за 1-2 рабочих дня по г. Алматы - 200 тг.</p>
-          <p>
+      <fieldset class="checkout-form__fieldset">
+        <legend>Способ доставки:</legend>
+        <label class="checkout-form__label">
+          <input
+            type="radio"
+            required
+            value="quick-delivery"
+            name="delivery-method"
+            checked
+          />
+          <p class="checkout-form__subtitle">
+            Быстрая доставка за 1-2 рабочих дня по г. Алматы - 200 тг.
+          </p>
+          <p class="checkout-form__description">
             Курьер доставит Ваш заказ за 1-2 рабочих дня с момента оформления
             заказа. Данный вид доставки возможен только в пределах
             административных границ г.Алматы, а если далее - то от 3000 тг и
             выше, что оговаривается отдельно.
           </p>
-          <input type="radio" required name="quick-delivery" checked />
         </label>
-        <label>
-          <p>
+        <label class="checkout-form__label">
+          <input
+            type="radio"
+            required
+            value="courier-delivery"
+            name="delivery-method"
+          />
+          <p class="checkout-form__subtitle">
             Доставка курьером при сумме заказа от 40000 тг по г. Алматы -
             Бесплатно.
           </p>
-          <p>
+          <p class="checkout-form__description">
             Срок доставки - от 2 до 7 рабочих дней с момента оформления заказа.
             Данный вид доставки действует в пределах административных границ г.
             Алматы..
           </p>
-          <input type="radio" required name="delivery-courer" />
         </label>
-        <label>
-          <p>Пункт выдачи - Бесплатно</p>
-          <p>
+        <label class="checkout-form__label">
+          <input
+            type="radio"
+            required
+            value="pickup-point"
+            name="delivery-method"
+          />
+          <p class="checkout-form__subtitle">Пункт выдачи - Бесплатно</p>
+          <p class="checkout-form__description">
             Самовывоз товара из магазина, расположенного по адресу: Казахстан,
             г. Алматы, ул. Щепеткова, 122. Срок хранения заказа - 3 дня.
           </p>
-          <input type="radio" required name="place-end" />
         </label>
-        <label>
-          <p>Доставка в отделение Почты г. Алматы - Бесплатно</p>
-          <p>
+        <label class="checkout-form__label">
+          <input
+            type="radio"
+            required
+            value="post-office-delivery"
+            name="delivery-method"
+          />
+          <p class="checkout-form__subtitle">
+            Доставка в отделение Почты г. Алматы - Бесплатно
+          </p>
+          <p class="checkout-form__description">
             Срок доставки — в среднем от 2 до 7 рабочих дней с момента
             оформления заказа. Срок хранения заказа в отделении Почты - 10
             рабочих дней.
           </p>
-          <input type="radio" required name="delivery-in-part-post-almaty" />
         </label>
       </fieldset>
-      <fieldset>
-        <legend>Способ оплаты</legend>
-        <label>
-          <p>Оплата с помощью Каспи</p>
-          <p>Доступная система: Каспи</p>
-          <p>Номер кошелька: +77077478212</p>
-          <p>
+      <fieldset class="checkout-form__fieldset">
+        <legend>Способ оплаты:</legend>
+        <label class="checkout-form__label">
+          <p class="checkout-form__subtitle">Оплата с помощью Каспи</p>
+          <p class="checkout-form__description">Доступная система: Каспи</p>
+          <p class="checkout-form__description">Номер кошелька: +77077478212</p>
+          <p class="checkout-form__description">
             При поступлении средств на кошелек Ваш заказ будет считаться
             оформленным.
           </p>
-          <input type="radio" required name="pay-caspy-bank" checked />
+          <input type="radio" required name="payment-method" value="pay-caspy-bank" checked />
         </label>
-        <label>
-          <p>Наличными при получении</p>
-          <p>
+        <label class="checkout-form__label">
+          <p class="checkout-form__subtitle">Наличными при получении</p>
+          <p class="checkout-form__description">
             Данный вид оплаты действует только для г.Алматы и его окрестностей
           </p>
-          <input type="radio" required name="cash" />
+          <input type="radio" required name="payment-method" value="cash" />
         </label>
       </fieldset>
       <fieldset>
-        <legend>Комментарий к заказу</legend>
+        <legend>Комментарий к заказу:</legend>
         <label>
           <p>Ваш комментарий</p>
           <textarea name="comments" id="comments"></textarea>
