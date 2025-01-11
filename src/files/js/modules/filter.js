a09a34a0f2047263d9d8baf83a5092d0f2a3a30a
Girey <larsvokul@mail.ru>
2025-01-11 16:40:20 +0300

    AAA-159 update

--- src/files/js/modules/filter.js
+++ src/files/js/modules/filter.js
@@ -1,15 +1,17 @@
 export function filterToggleMenu() {
-  const filterBtn = document.getElementById("filter-btn");
-  const filterCatalog = document.getElementById("filter-catalog");
-  const filterBtnClose = document.getElementById("filter-btn-close");
+  const filterBtn = document.getElementById('filter-btn');
+  const filterCatalog = document.getElementById('filter-catalog');
+  const filterBtnClose = document.getElementById('filter-btn-close');
+  let flag = false;
 
-  filterBtn.addEventListener("click", () => {
-    filterCatalog.classList.toggle("open");
-    filterBtnClose.style.display = "flex";
+  filterBtn.addEventListener('click', () => {
+    filterCatalog.classList.toggle('open');
+    filterBtnClose.style.display = 'flex';
+    flag = true;
   });
 
-  filterBtnClose.addEventListener("click", () => {
-    filterCatalog.classList.toggle("open");
-    filterBtnClose.style.display = "none";
+  filterBtnClose.addEventListener('click', () => {
+    filterCatalog.classList.toggle('open');
+    filterBtnClose.style.display = 'none';
   });
 }
