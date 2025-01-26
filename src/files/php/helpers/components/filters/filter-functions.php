5cddba0f92b27b6b79306662b1affdb5bb08cfec
Izosimov_Andrew <chernigovsky108@gmail.com>
2025-01-26 20:29:24 +0300

    AAA-160 fixies sorting in park-systems, filters

--- src/files/php/helpers/components/filters/filter-functions.php
+++ src/files/php/helpers/components/filters/filter-functions.php
@@ -2,81 +2,28 @@
 class FilterFunctions
 {
     private $data_filters;
-    private $products;
 
-    public function __construct($data_filters, $products)
+    public function __construct($data_filters)
     {
         $this->data_filters = $data_filters;
-        $this->products = $products;
     }
 
-    /**
-     * Подсчитывает количество товаров для каждого значения options-filters.
-     *
-     * @return array
-     */
-    public function countOptionsFilters()
-    {
-        $count = [];
-
-        // Проходим по всем категориям и товарам
-        foreach ($this->products['category'] as $items) {
-            foreach ($items as $product) {
-                // Проверяем, есть ли у товара поле options-filters
-                if (isset($product['options-filters']) && is_array($product['options-filters'])) {
-                    // Проходим по каждому значению options-filters
-                    foreach ($product['options-filters'] as $filter) {
-                        // Увеличиваем счетчик для текущего фильтра
-                        if (isset($count[$filter])) {
-                            $count[$filter]++;
-                        } else {
-                            $count[$filter] = 1;
-                        }
-                    }
-                }
-            }
-        }
-        error_log(print_r($count, true));
-        return $count;
-    }
-
-    /**
-     * Рендерит блок фильтров.
-     *
-     * @return string
-     */
     public function renderFilters()
     {
-        // Подсчитываем количество товаров для каждого фильтра
-        $count = $this->countOptionsFilters();
 
         ob_start();
         ?>
         <div class="filter-functions">
             <p class="filter-functions__title">Функции</p>
             <?php foreach ($this->data_filters as $category): ?>
-                <?php
-                // Получаем значение 'options-filters' из $category
-                $filterKey = $category['options-filters'] ?? null;
-
-                // Если ключ существует в массиве $count, используем его значение, иначе 0
-                $filterCount = 0;
-                if (is_array($filterKey)) {
-                    foreach ($filterKey as $key) {
-                        $filterCount += $count[$key] ?? 0;
-                        error_log(print_r($filterCount, true) . ' : filterCount');
-                    }
-                } else {
-                    $filterCount = $count[$filterKey] ?? 0;
-                }
-                ?>
+     
                 <div class="filter-functions__item">
                     <label class="filter-functions__item-element">
                         <input type="checkbox" class="filter-functions__checkbox" id="<?= htmlspecialchars($category['name']) ?>"
                                name="<?= htmlspecialchars($category['name']) ?>">
                         <span class="filter-functions__item-title"><?= htmlspecialchars($category['text']) ?></span>
                     </label>
-                    <span class="filter-functions__count"><?= $filterCount ?></span>
+                    <span class="filter-functions__count"><?= $category['count'] ?></span>
                 </div>
             <?php endforeach; ?>
         </div>
--- src/files/php/helpers/components/filters/filters.php
+++ src/files/php/helpers/components/filters/filters.php
@@ -15,7 +15,6 @@ class Filters
 {
     private $filterCost;
     private $filterFunctions;
-
     private $filterBasic;
 
     private $path;
@@ -24,14 +23,16 @@ class Filters
     public function __construct($data_filters, $products)
     {
         $this->filterCost = new FilterCost();
-        $this->filterFunctions = new FilterFunctions($data_filters, $products);
-        $this->filterBasic = new filterBasic();
+        $this->filterFunctions = new FilterFunctions($data_filters);
+        $this->filterBasic = new FilterBasic();
+
         $variables = new SetVariables();
         $variables->setVar();
-        $path = $variables->getPathFileURL();
-        $this->path = $path;
+        $this->path = $variables->getPathFileURL();
 
+        // Инициализация массива для подсчета товаров
         $this->filters_products_count = [];
+
         if (!empty($products)) {
             // Проходим по всем категориям и товарам
             foreach ($products['category'] as $items) {
@@ -52,24 +53,39 @@ class Filters
             }
         }
     }
-    public function renderFilters()
+
+    public function renderFilters($filterBasic = true, $filterCost = true, $filterFunctions = true, $pathSend = '/files/php/pages/catalog/catalog.php')
     {
         ob_start();
         ?>
-    <button class="filter-button" type="button" id="filter-btn"
-      style="background-image: url(<?= htmlspecialchars($this->path . '/assets/images/vectors/filters.svg'); ?>);">Фильтр</button>
-    <button class="filter-button-close" type="button" id="filter-btn-close"><span class="visually-hidden">скрыть
-        фильтры</span></button>
-    <form class="filter-form open" id="filter-catalog" action="<?= $this->path;?>/files/php/pages/catalog/catalog.php" method="get">
-      <?php echo $this->filterBasic->renderFilters($this->filters_products_count); ?>
-      <?php echo $this->filterCost->renderFilters(); ?>
-      <?php echo $this->filterFunctions->renderFilters($this->filters_products_count); ?>
-      <div class="filters__buttons">
-        <button type="submit" class="button y-button-primary">Применить</button>
-        <button type="reset" class="button y-button-secondary">Сбросить</button>
-      </div>
-    </form>
-    <?php
+        <button class="filter-button" type="button" id="filter-btn"
+            style="background-image: url(<?= htmlspecialchars($this->path . '/assets/images/vectors/filters.svg'); ?>);">Фильтр</button>
+        <button class="filter-button-close" type="button" id="filter-btn-close">
+            <span class="visually-hidden">скрыть фильтры</span>
+        </button>
+        <form class="filter-form open" id="filter-catalog" action="<?= $this->path . $pathSend; ?>" method="get">
+            <?php
+            // Рендерим базовые фильтры
+            if ($filterBasic === true) {
+                echo $this->filterBasic->renderFilters($this->filters_products_count);
+            }
+
+        // Рендерим фильтры по цене
+        if ($filterCost === true) {
+            echo $this->filterCost->renderFilters();
+        }
+
+        // Рендерим фильтры по функциям
+        if ($filterFunctions === true) {
+            echo $this->filterFunctions->renderFilters();
+        }
+        ?>
+            <div class="filters__buttons">
+                <button type="submit" class="button y-button-primary">Применить</button>
+                <button type="reset" class="button y-button-secondary">Сбросить</button>
+            </div>
+        </form>
+        <?php
         return ob_get_clean();
     }
 }
