1422e8eba59a26599ff0539536b0aad616127d56
Girey <larsvokul@mail.ru>
2025-01-29 03:21:27 +0300

    AAA-173 fix empty tabs

--- src/files/php/sections/card-tabs.php
+++ src/files/php/sections/card-tabs.php
@@ -38,6 +38,10 @@ function isActiveClassTabContent($index)
         <div class="tab__buttons">
             <?php $index = 0; ?>
             <?php foreach ($product_tabs as $tab_title => $tab_content): ?>
+                <?php if (empty($tab_content)) { 
+                    error_log(print_r($index, true)); 
+                continue;
+             } ?>
                 <button type="button" class="tab__button <?= isActiveClassTab($index) ?> y-button-secondary"
                         data-tab="<?= $tab_title; ?>"><?= $tab_title; ?></button>
                 <?php $index++; ?>
@@ -63,7 +67,6 @@ function isActiveClassTabContent($index)
                             <?php endforeach; ?>
                         </ul>
                     <?php endif; ?>
-
                     <!-- Список items-service -->
                     <?php if (isset($tab_content['items-service'])): ?>
                         <p class="tab__text">Удобный сервис</p>
@@ -91,7 +94,7 @@ function isActiveClassTabContent($index)
                             </li>
                         <?php endforeach; ?>
                     </ul>
-                <?php endif; ?>
+                <?php endif; ?>  
                 <?php $index++; ?>
             <?php endforeach; ?>
         </div>
