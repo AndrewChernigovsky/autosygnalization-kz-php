9444478293482b97577787f7f9ef283b69d81f1b
Girey-m <larsvokul@mail.ru>
2025-02-10 23:04:58 +0300

    Revert "Revert "AAA-194 add new class FilterRenders and remove other files""

--- src/files/php/pages/parking-systems/parking-systems.php
+++ src/files/php/pages/parking-systems/parking-systems.php
@@ -5,7 +5,6 @@ include_once __DIR__ . '/../../data/select.php';
 include_once __DIR__ . '/../../data/products.php';
 include_once __DIR__ . '/../../helpers/classes/setVariables.php';
 include_once __DIR__ . '/../../helpers/components/filters/filters.php';
-include_once __DIR__ . '/../../helpers/components/filters/sorting.php';
 include_once __DIR__ . '/../../helpers/components/setup.php';
 include_once __DIR__ . '/../../helpers/components/product.php';
 include_once __DIR__ . '/../../helpers/components/article.php';
@@ -27,12 +26,10 @@ $head_path = $docROOT . $path . '/files/php/layout/head.php';
 $title = 'Парковочные системы | Auto Security';
 
 include_once $head_path;
-include_once $docROOT . $path . '/files/php/data/filters.php';
 include_once $docROOT . $path . '/files/php/data/products.php';
 
 $head = new Head($title, [], []);
-$filters = new Filters($data_categories_filters, $products);
-$sorting = new Sorting();
+$filters_render = new FiltersRender($products, "auto");
 $article = new Article();
 $articleData = new ArticleData();
 $select = new Select();
@@ -113,7 +110,7 @@ echo $head->setHead();
       <div class="catalog__wrapper parking-system">
         <aside class="aside">
           <p>Стоимость</p>
-          <?= $filters->renderFilters(false, true, false, '/files/php/pages/parking-systems/parking-systems.php'); ?>
+          <?= $filters_render->renderFilters(false, true, false, '/files/php/pages/parking-systems/parking-systems.php'); ?>
         </aside>
         <div class="catalog__products">
           <?= $select->createComponent($selectData->getSelectData()) ?>
