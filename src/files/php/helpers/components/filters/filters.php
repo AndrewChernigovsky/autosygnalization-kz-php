89543a927a1f28322b6ec78b9046963cc5016aff
Girey <larsvokul@mail.ru>
2025-02-10 22:59:02 +0300

    AAA-194 add new class FilterRenders and remove other files

--- src/files/php/helpers/components/filters/filters.php
+++ src/files/php/helpers/components/filters/filters.php
@@ -8,44 +8,177 @@ $path = $docROOT . $variables->getPathFileURL();
 
 
 include_once $path . '/files/php/data/products.php';
-include_once $path . '/files/php/helpers/components/filters/filter-cost.php';
-include_once $path . '/files/php/helpers/components/filters/filter-functions.php';
-include_once $path . '/files/php/helpers/components/filters/filter-basic.php';
 
 
-class Filters
+class FiltersRender
 {
-    private $filterCost;
-    private $filterFunctions;
-    private $filterBasic;
     private $filter_correct;
-    
-
     private $path;
     private $filters_products_count;
+    private $get_params;
+    
+    private $data_filters;
+    private array $products;
 
-    public function __construct($products, $filter_correct = null)
+    public function __construct(array $products = [], $filter_correct = null,$get_params = null, $data_filters = null)
     {
-        $this->filter_correct =  $filter_correct;
-        $this->filterCost = new FilterCost();
-        $this->filterFunctions = new FilterFunctions($products, $filter_correct);
-        $this->filterBasic = new FilterBasic();
+
+        $this->data_filters = !empty($data_filters) ? $data_filters : [
+            ["text" => "Автозапуск", "name" => "autosetup", "count" => "1"],
+            ["text" => "УПРАВЛЕНИЕ С ТЕЛЕФОНА", "name" => "control-phone", "count" => "1"],
+            ["text" => "БЕСПЛАТНЫЙ МОНИТОРИНГ", "name" => "free-monitoring", "count" => "1"],
+            ["text" => "УМНАЯ АВТОРИЗАЦИЯ ПО BLUETOOTH SMART", "name" => "bluetooth-smart", "count" => "1"],
+            ["text" => "БЛОКИРОВКА ДВИГАТЕЛЯ ПО CAN", "name" => "block-engine-can", "count" => "1"],
+            ["text" => "УПРАВЛЕНИЕ ПРЕДПУСКОВЫМ ПОДОГРЕВОМ", "name" => "control-before-start", "count" => "1"],
+            ["text" => "ДАННЫЕ О ПРОБЕГЕ И УРОВНЕ ТОПЛИВА", "name" => "data-level-bensin", "count" => "1"],
+            ["text" => "УМНАЯ АВТОДИАГНОСТИКА", "name" => "smart-diagnostic", "count" => "1"],
+        ];
+        $this->get_params = !empty($get_params) ? $get_params : $_GET;
+        $this->filter_correct = $filter_correct;
 
         $variables = new SetVariables();
         $variables->setVar();
         $this->path = $variables->getPathFileURL();
-        // Инициализация массива для подсчета товаров
+        $this->products = $products;
         $this->filters_products_count = [];
-        if (!empty($products)) {
-            // Проходим по всем категориям и товарам
-            foreach ($products['category'] as $items) {
+
+
+    
+    }
+
+    public function renderFiltersFunctions()
+    {
+        $count = [];
+
+        
+        if ($this->filter_correct !== null && $this->get_params !== null) {
+            foreach ($this->products['category'] as $items) {
                 foreach ($items as $product) {
-                    // Проверяем, есть ли у товара поле options-filters
+                    $match = true;
+                    $minCost = $this->get_params['min-value-cost'] ?? 0;
+                    $maxCost = $this->get_params['max-value-cost'] ?? PHP_INT_MAX;
+
+                    if ($product['price'] < $minCost || $product['price'] > $maxCost) {
+                        $match = false;
+                    }
+                    
+                    $active_params_arr = [];
+                    foreach ($this->get_params as $key => $value) {
+                        if ($value === 'on') {
+                            $active_params_arr[] = $key;
+                        }
+                    }
+
+                    if ($match && isset($product['options-filters']) && is_array($product['options-filters']) && in_array($this->filter_correct, $product['autosygnals']) && empty(array_diff($active_params_arr, $product['options-filters']))) {
+                        foreach ($product['options-filters'] as $filter) {
+                            $count[$filter] = ($count[$filter] ?? 0) + 1;
+                        }
+                    }
+                    
+                }
+            }
+        } elseif ($this->get_params !== null) {
+            foreach ($this->products['category'] as $items) {
+                foreach ($items as $product) {
+                    $match = true;
+                    $minCost = $this->get_params['min-value-cost'] ?? 0;
+                    $maxCost = $this->get_params['max-value-cost'] ?? PHP_INT_MAX;
+
+                    if ($product['price'] < $minCost || $product['price'] > $maxCost) {
+                        $match = false;
+                    }
+
+                    $active_params_arr = [];
+                    foreach ($this->get_params as $key => $value) {
+                        if ($value === 'on') {
+                            $active_params_arr[] = $key;
+                        }
+                    }
+
+                    if ($match && isset($product['options-filters']) && is_array($product['options-filters']) && empty(array_diff($active_params_arr, $product['options-filters']))) {
+                        foreach ($product['options-filters'] as $filter) {
+                            $count[$filter] = ($count[$filter] ?? 0) + 1;
+                        }
+                    }
+                }
+            }
+        }
+    
+        foreach ($this->data_filters as $index => $category) {
+            $filterKey = $category['name'];
+            $this->data_filters[$index]['count'] = $count[$filterKey] ?? 0;
+        }
+
+        ob_start();
+        ?>
+        <div class="filter-functions">
+            <p class="filter-functions__title">Функции</p>
+            <?php foreach ($this->data_filters as $category): ?>
+                <div class="filter-functions__item">
+                    <label class="filter-functions__item-element">
+                        <input type="checkbox" class="filter-functions__checkbox" id="<?= htmlspecialchars($category['name']) ?>"
+                               name="<?= htmlspecialchars($category['name']) ?>">
+                        <span class="filter-functions__item-title"><?= htmlspecialchars($category['text']) ?></span>
+                    </label>
+                    <span class="filter-functions__count"><?= $category['count'] ?></span>
+                </div>
+            <?php endforeach; ?>
+        </div>
+        <?php
+        return ob_get_clean();
+    }
+    public function renderFiltersCost()
+    {
+        ob_start();
+        ?>
+            <div class="double-range-wrapper filter-cost">
+                <div class="range-slider-container">
+                    <div class="range-container">
+                    <div class="range-scale">
+                        <div class="range-progress"></div>
+                    </div>
+                    <div class="range-wrapper">
+                        <input type="range" class="range-input range-min" min="100" max="300000" step="100"/>
+                        <input type="range" class="range-input range-max" />
+                    </div>
+                    </div>
+                    <div class="number-container">
+                    <div class="numbers-wrapper">
+                        <input type="number" class="number-input input-min" min="100" max="300000" step="100" name="min-value-cost"/>
+                    </div>
+                    <div class="numbers-wrapper">
+                        <input type="number" class="number-input input-max" min="100" max="300000" step="100" name="max-value-cost"/>
+                    </div>
+                    </div>
+                </div>
+            </div>
+
+        <?php
+        
+        return ob_get_clean();
+    }
+    public function renderFiltersBasic()
+    {
+        if (!empty($this->products)) {
+            foreach ($this->products['category'] as $items) {
+                foreach ($items as $product) {
                     if ($this->filter_correct !== null) {
-                        if (isset($product['options-filters']) && is_array($product['options-filters']) && in_array($this->filter_correct, $product['autosygnals'])) {
-                            // Проходим по каждому значению options-filters
+                        $match = true;
+                        $minCost = $this->get_params['min-value-cost'] ?? 0;
+                        $maxCost = $this->get_params['max-value-cost'] ?? PHP_INT_MAX;
+    
+                        if ($product['price'] < $minCost || $product['price'] > $maxCost) {
+                            $match = false;
+                        }
+                        
+                        $active_params_arr = [];
+                        foreach ($this->get_params as $key => $value) {
+                            if ($value === 'on') {
+                                $active_params_arr[] = $key;
+                            }
+                        }
+                        if ($match && isset($product['options-filters']) && is_array($product['options-filters']) && in_array($this->filter_correct, $product['autosygnals']) && empty(array_diff($active_params_arr, $product['options-filters']))) {
                             foreach ($product['options-filters'] as $filter) {
-                                // Увеличиваем счетчик для текущего фильтра
                                 if (isset($this->filters_products_count[$filter])) {
                                     $this->filters_products_count[$filter]++;
                                 } else {
@@ -54,14 +187,25 @@ class Filters
                             }
                         }
                     } else {
-                        if (isset($product['options-filters']) && is_array($product['options-filters'])) {
-                            // Проходим по каждому значению options-filters
+                        $match = true;
+                        $minCost = $this->get_params['min-value-cost'] ?? 0;
+                        $maxCost = $this->get_params['max-value-cost'] ?? PHP_INT_MAX;
+    
+                        if ($product['price'] < $minCost || $product['price'] > $maxCost) {
+                            $match = false;
+                        }
+                        $active_params_arr = [];
+                        foreach ($this->get_params as $key => $value) {
+                            if ($value === 'on') {
+                                $active_params_arr[] = $key;
+                            }
+                        }
+                        if ($match && isset($product['options-filters']) && is_array($product['options-filters']) && empty(array_diff($active_params_arr, $product['options-filters']))) {
                             foreach ($product['options-filters'] as $filter) {
-                                // Увеличиваем счетчик для текущего фильтра
                                 if (isset($this->filters_products_count[$filter])) {
                                     $this->filters_products_count[$filter]++;
                                 } else {
-                                    $this->filters_products_count[$filter] = 0;
+                                    $this->filters_products_count[$filter] = 1;
                                 }
                             }
                         }
@@ -70,8 +214,28 @@ class Filters
                 }
             }
         }
-    }
+        ob_start();
+        ?>
+            <div class="filter-basic">
+                <div class="filter-basic__item">
+                    <label for="vnedorojnik" class="filter-basic__item-element">
+                    <input type="checkbox" class="filter-basic__checkbox" id="vnedorojnik" name="vnedorojnik">
+                    <span class="filter-basic__item-title">Для внедорожника</span>
+                    </label>
+                    <span class="filter-basic__count"><?= $this->filters_products_count['vnedorojnik'] ?? 0?></span>
+                </div>
+                <div class="filter-basic__item">
+                    <label for="legkoe-avto" class="filter-basic__item-element">
+                    <input type="checkbox" class="filter-basic__checkbox" id="legkoe-avto" name="legkoe-avto">
+                    <span class="filter-basic__item-title">Для легкое авто</span>
+                    </label>
+                    <span class="filter-basic__count"><?= $this->filters_products_count['legkoe-avto'] ?? 0?></span>
+                </div>
+            </div>
+        <?php
 
+        return ob_get_clean();
+    }
     public function renderFilters($filterBasic = true, $filterCost = true, $filterFunctions = true, $pathSend = '/files/php/pages/catalog/catalog.php')
     {
         ob_start();
@@ -83,21 +247,18 @@ class Filters
         </button>
         <form class="filter-form open" id="filter-catalog" action="<?= $this->path . $pathSend; ?>" method="get">
             <?php
-            // Рендерим базовые фильтры
-            if ($filterBasic === true) {
-                echo $this->filterBasic->renderFilters($this->filters_products_count);
-            }
+                if ($filterBasic === true) {
+                    echo $this->renderFiltersBasic();
+                }
 
-        // Рендерим фильтры по цене
-        if ($filterCost === true) {
-            echo $this->filterCost->renderFilters();
-        }
+                if ($filterCost === true) {
+                    echo $this->renderFiltersCost();
+                }
 
-        // Рендерим фильтры по функциям
-        if ($filterFunctions === true) {
-            echo $this->filterFunctions->renderFilters();
-        }
-        ?>
+                if ($filterFunctions === true) {
+                    echo $this->renderFiltersFunctions();
+                }
+            ?>
             <div class="filters__buttons">
                 <button type="submit" class="button y-button-primary">Применить</button>
                 <button type="reset" class="button y-button-secondary">Сбросить</button>
