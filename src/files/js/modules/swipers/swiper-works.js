b38bb2362e1cd2c782d682b67dc8bace646fbf3c
Maria <lady.mescheryakowa@yandex.ru>
2025-02-26 23:08:15 +0300

    AAA-210

--- src/files/js/modules/swipers/swiper-works.js
+++ src/files/js/modules/swipers/swiper-works.js
@@ -1,11 +1,12 @@
 import Swiper from 'swiper';
-import { Pagination, Autoplay } from 'swiper/modules';
+import { Pagination, Autoplay, Zoom } from 'swiper/modules';
 
 const worksSwiper = new Swiper('.swiper-works', {
-    modules: [Pagination, Autoplay],
+    modules: [Pagination, Autoplay, Zoom],
     slidesPerView: 3,
     spaceBetween: 30,
     loop: true,
+    zoom: true,
     autoplay: {
         delay: 3000,
         disableOnInteraction: false,
@@ -25,6 +26,24 @@ const worksSwiper = new Swiper('.swiper-works', {
             slidesPerView: 3,
         }
     }
+});
+
+document.querySelectorAll('.works__slide-image img').forEach(img => {
+    img.addEventListener('click', function(e) {
+        e.preventDefault();
+        const modal = document.createElement('div');
+        modal.className = 'modal-image';
+        
+        const modalImg = document.createElement('img');
+        modalImg.src = this.src;
+        
+        modal.appendChild(modalImg);
+        document.body.appendChild(modal);
+        
+        modal.addEventListener('click', function() {
+            this.remove();
+        });
+    });
 });
 
 export default worksSwiper;
