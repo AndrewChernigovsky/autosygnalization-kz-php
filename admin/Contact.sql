253d9be866c99c717b21ef7e6cebc28c61d35e5a
Girey-m <larsvokul@mail.ru>
2025-07-16 18:56:43 +0300

    add contact api

--- admin/Contact.sql
+++ admin/Contact.sql
@@ -10,6 +10,6 @@ CREATE TABLE `Contacts` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Начальные данные: добавление основных контактов
-INSERT INTO `Contacts` (`contact_id`, `title`, `phone`, `link`, `created_at`, `updated_at`) VALUES
-(1, 'Основной', '+7 707 747 8212', 'tel:+77077478212', '2025-07-15 15:23:00', '2025-07-15 15:23:00'),
-(2, 'Запасной', '+7 701 747 8212', 'tel:+77017478212', '2025-07-15 15:23:00', '2025-07-15 15:23:00');
+INSERT INTO `Contacts` (`title`, `phone`, `link`) VALUES
+('Основной', '+7 707 747 8212', 'tel:+77077478212'),
+('Запасной', '+7 701 747 8212', 'tel:+77017478212');
