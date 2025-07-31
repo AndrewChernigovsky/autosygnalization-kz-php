3093c5f267547ee35db704a8f1569e5ccb96c4a7
Andrew Izosimov <chernigovsky108@gmail.com>
2025-07-31 03:24:51 +0300

    products added prices

--- src/server/php/pages/price/price.php
+++ src/server/php/pages/price/price.php
@@ -41,40 +41,41 @@ echo $head->setHead();
           <h1 class="price__title">Прайс</h1>
           <h2 class="price__subtitle">Прайс по оборудованию Starline и цены на установку:<span>*</span></h2>
           <ul class="price__list list-style-none">
-  <?php foreach ($prices as $price): ?>
-    <li class="price__item">
-      <details class="price__details product-cart">
-        <summary class="price__summary">
-          <p class="price__item-title" role="term" aria-details="faq-1">
-            <?= htmlspecialchars($price['title']); ?>
-          </p>
-          <div class="price__item-box">
-            <span class="price__item-product"><?= htmlspecialchars($price['price']); ?></span>
-            <span class="price__item-currency"><?= htmlspecialchars($price['currency']); ?></span>
-          </div>
-          <?php if (!empty($price['prices'])): ?>
-            <?php foreach ($price['prices'] as $service): ?>
-              <p class="price__item-price">
-                установка от <?= htmlspecialchars($service['price']) ?>*
-              </p>
+            <?php foreach ($prices as $price): ?>
+              <li class="price__item">
+                <details class="price__details product-cart">
+                  <summary class="price__summary">
+                    <p class="price__item-title" role="term" aria-details="faq-1">
+                      <?= htmlspecialchars($price['title']); ?>
+                    </p>
+                    <div class="price__item-box">
+                      <span class="price__item-product"><?= htmlspecialchars($price['price']); ?></span>
+                      <span class="price__item-currency"><?= htmlspecialchars($price['currency']); ?></span>
+                    </div>
+                    <?php if (!empty($price['prices'])): ?>
+                      <?php foreach ($price['prices'] as $service): ?>
+                        <p class="price__item-price">
+                          установка от <?= htmlspecialchars($service['price']) ?>
+                          <?= htmlspecialchars($price['currency']) ?>
+                        </p>
+                      <?php endforeach; ?>
+                    <?php endif; ?>
+                  </summary>
+                </details>
+                <div class="price__content" id="faq-1" role="definition">
+                  <div class="price__content-body">
+                    <?php if (!empty($price['prices'])): ?>
+                      <?php foreach ($price['prices'] as $service): ?>
+                        <div class="service-description">
+                          <?= $service['content'] // content — это HTML, не экранируй! ?>
+                        </div>
+                      <?php endforeach; ?>
+                    <?php endif; ?>
+                  </div>
+                </div>
+              </li>
             <?php endforeach; ?>
-          <?php endif; ?>
-        </summary>
-      </details>
-      <div class="price__content" id="faq-1" role="definition">
-        <div class="price__content-body">
-          <?php if (!empty($price['prices'])): ?>
-            <?php foreach ($price['prices'] as $service): ?>
-              <div class="service-description">
-                <?= $service['content'] // content — это HTML, не экранируй! ?>
-              </div>
-            <?php endforeach; ?>
-          <?php endif; ?>
-        </div>
-      </div>
-    </li>
-  <?php endforeach; ?>
-</ul>
+          </ul>
         </div>
       </div>
     </section>
